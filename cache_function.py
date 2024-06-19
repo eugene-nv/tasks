e92b4e93ee49b310d07806ff128eca618f6b7361
eugene-nv <naumkin.e.v@yandex.ru>
2024-06-19 11:38:13 +0500

    add cache_function.py

---
+++ cache_function.py
@@ -0,0 +1,30 @@
+import random
+
+
+def cache_function():
+    count = 0
+    value = random.randint(1, 111)
+
+    def inner():
+        nonlocal count, value
+
+        if count != 3:
+            count += 1
+            return value
+
+        else:
+            count = 1
+            value = random.randint(1, 111)
+            return value
+
+    return inner
+
+
+c = cache_function()
+
+print(c())
+print(c())
+print(c())
+print(c())
+
+
